1a4f086e568b23d735ce1d2be17b4b8e2a4c8479
ojh6t3k <ojh6t3k@naver.com>
2014-09-02 14:31:26 +0900

    프로젝트 개발 준비 완료
    -UnityRobot 추가 (v1.0.0b3 - 개발중)

---
+++ src/Assets/UnityRobot/Scripts/MotorModule.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace UnityRobot
+{
+	[AddComponentMenu("UnityRobot/MotorModule")]
+	public class MotorModule : ModuleProxy
+	{
+		private short _speed;
+		
+		void Awake()
+		{
+		}
+		
+		// Use this for initialization
+		void Start ()
+		{
+			
+		}
+		
+		// Update is called once per frame
+		void Update ()
+		{
+		}
+		
+		public override void Reset ()
+		{
+			_speed = 0;
+			canUpdate = true;
+		}
+		
+		public override void Action ()
+		{
+		}
+		
+		public override void OnPop ()
+		{
+		}
+		
+		public override void OnPush ()
+		{
+			Push (_speed);
+		}
+		
+		public int speed
+		{
+			get
+			{
+				return (int)_speed;
+			}
+			set
+			{
+				short newValue = (short)Mathf.Clamp(value, -100, 100);
+				if(_speed != newValue)
+				{
+					_speed = newValue;
+					canUpdate = true;
+				}
+			}
+		}
+	}
+}
